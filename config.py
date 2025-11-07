e866a3b2ff29211463f4deb4d7ae11627321b8dc
菜逼程序员 <nismison@gmail.com>
2025-11-07 16:53:46 +0800

    feat(config): 引入环境变量配置支持- 添加 dotenv依赖以支持 .env 文件加载- 修改 config.py 以从环境变量读取配置 - 根据 ENV 环境变量值切换开发/生产环境配置- 更新 .gitignore 忽略 /apks/ 目录和 /.env 文件 - 调整图库本地存储配置的环境判断逻辑

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 import logging.config
+from dotenv import load_dotenv
+
+load_dotenv()
+IS_DEV = os.getenv("ENV") == "dev"
 
 # ==================== 自动工单配置 ====================
 # 基础 Token (从环境变量读取)
@@ -16,13 +20,14 @@ HEADERS_BASE = {
 }
 
 # ==================== 图库本地存储配置 ====================
-# 开发环境
-BASE_URL = "http://192.168.245.233:5001"
-IMMICH_URL = 'https://immich.zytsy.icu/api'
-
-# 正式环境
-# BASE_URL = "https://api.zytsy.icu"
-# IMMICH_URL = 'http://127.0.0.1:2283/api'
+if IS_DEV:
+    # 开发环境
+    BASE_URL = "http://192.168.245.233:5001"
+    IMMICH_URL = 'https://immich.zytsy.icu/api'
+else:
+    # 正式环境
+    BASE_URL = "https://api.zytsy.icu"
+    IMMICH_URL = 'http://127.0.0.1:2283/api'
 
 # ==================== immich配置 ====================
 IMMICH_API_KEY = '9zYuNvGF9ZGTOrqULYL5hoWc6JfRBx7wdN44tdb2w'
