6caca18b8fc3aa766b1ac3bbe27df33fe9678958
菜逼程序员 <nismison@gmail.com>
2025-11-15 19:23:37 +0800

    feat(immich): 添加系统路径配置支持

--- ql_corn/immich_statistics.py
+++ ql_corn/immich_statistics.py
@@ -1,6 +1,12 @@
+import os
+import sys
 from datetime import datetime, timedelta
+
 from apis.immich_api import IMMICHApi
 from utils.notification import Notify
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(ROOT_DIR)
 
 immich_api = IMMICHApi()
 
