58ba3d34432204b2fcadc91a03b931826b73999b
Nismison <nismison@gmail.com>
2026-01-10 09:29:38 +0800

    refactor(notification): 重构通知模块实现

--- checkin_server.py
+++ checkin_server.py
@@ -1,35 +1,15 @@
 import json
 import random
-import threading
 import time
 from datetime import datetime
 
 import requests
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
-from wxpusher import WxPusher
 
-from utils.logger import log_line
 from config import TZ
-
-
-class Notify:
-    def __init__(self):
-        self.uids = ['UID_0UhoJ977fvwsJhzXokMmhzgIqFRZ']
-        self.token = 'AT_a5ARmQl4Mi8mCjv6xImNDesNfjSla8OW'
-
-    def _send_sync(self, text: str) -> None:
-        try:
-            WxPusher.send_message(text, uids=self.uids, token=self.token)
-        except Exception:
-            # “尝试发送”：失败无所谓，直接吞掉
-            pass
-
-    def send(self, text: str) -> None:
-        # 异步发送：不阻塞主流程
-        t = threading.Thread(target=self._send_sync, args=(text,), daemon=True)
-        t.start()
-
+from utils.logger import log_line
+from utils.notification import Notify
 
 notify = Notify()
 
